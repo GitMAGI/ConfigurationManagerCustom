a320ef1e68f24294bd83b70f39157ada9f187d7c
Leonardo Paglionico <paglionico@seminabit.com>
2017-07-04 15:57:07 +0200

    Init and FInal

---
+++ ConfigurationManagerCustom/ConfigurationManagerCustom/ConfigurationManagerCustom.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public sealed class ConfigurationManagerCustom
+{
+    public ConfigurationManagerCustom(string filename)
+    {
+        System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+        try
+        {
+            doc.Load(filename);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+        try
+        {
+            System.Xml.XmlNodeList nodes = doc.DocumentElement.SelectNodes("/configuration/appSettings/add");
+            if (nodes == null)
+                throw new NullReferenceException(string.Format("No Add Nodes into Configuration file '{0}'", filename));
+
+            Dictionary<string, string> tmp = new Dictionary<string, string>();
+
+            foreach (System.Xml.XmlNode node in nodes)
+            {
+                if (tmp == null)
+                    tmp = new Dictionary<string, string>();
+                tmp.Add(node.Attributes["key"].InnerText, node.Attributes["value"].InnerText);
+            }
+
+            if (tmp != null)
+                this.AppSettings = new ReadOnlyDictionary<string, string>(tmp);
+
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+    }
+
+    public ReadOnlyDictionary<string, string> AppSettings;
+}
